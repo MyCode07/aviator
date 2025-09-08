9c711a1d8dc9f3085ee56a31379ffeb38ee6cba0
MyCode07 <erem19992015@mail.ru>
2025-09-08 11:59:24 +0300

    add all

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,10 @@
-import './utils/menu.js';
 import './utils/sliders.js';
+import './utils/menu.js';
 import './utils/casino.js';
 import './utils/tabs.js';
 import './utils/faq.js';
-import './utils/accordeon.js';
 import './utils/popup.js';
-
+import './utils/accordeon.js';
 
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
